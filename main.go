5be2fb618715df541e54b7e7a6ad7138b9be86f6
Melodie Ribes <melodie.ribes@desamis.co.jp>
2021-06-01 16:01:16 +0900

    Handson

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s", r.Method, r.RequestURI)
-	w.Write([]byte("こんにちは！"))
+	// w.Write([]byte("こんにちは！"))
+	t := template.Must(template.ParseFiles("template/index.gohtml", "template/_menu.gohtml"))
+	if err := t.Execute(w, struct {
+		UserName string
+		Time     time.Time
+	}{
+		"ゲスト",
+		time.Now(),
+	}); err != nil {
+		log.Printf("テンプレート %s の実行に失敗！: %v", t.Name(), err)
+		http.Error(w, "内部エラーです", http.StatusInternalServerError)
+	}
 }
+
+func handleSecret(w http.ResponseWriter, r *http.Request) {
+	user, password, _ := r.BasicAuth()
+	if user != "user" || password != "password" {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+		http.Error(w, "認証に失敗しました", http.StatusUnauthorized)
+		return
+	}
+	log.Printf("%s %s", r.Method, r.RequestURI)
+	w.Write([]byte("秘密のページです！"))
+}
+
 func main() {
 	port := os.Getenv("PORT") // 実行時に Heroku が指定するポート番号を取得
 	if len(port) == 0 {
 		port = "8080" // ローカルで実行するときのポート番号を指定
 	}
 	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/secret", handleSecret)
 	log.Printf("ポート %s で待ち受けを開始します...", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Printf("サーバーが異常終了しました: %v", err)
