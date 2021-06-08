e2049b4b8e24290ee157058671819c035a67fbcf
Melodie Ribes <melodie.ribes@desamis.co.jp>
2021-06-08 11:25:13 +0900

    Add a page

--- main.go
+++ main.go
@@ -35,6 +35,15 @@ func handleSecret(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("秘密のページです！"))
 }
 
+func handleHolidays(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", r.Method, r.RequestURI)
+	t := template.Must(template.ParseFiles("template/holidays.gohtml", "template/_menu.gohtml"))
+	if err := t.Execute(w, nil); err != nil {
+		log.Printf("テンプレート %s の実行に失敗！: %v", t.Name(), err)
+		http.Error(w, "内部エラーです", http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	port := os.Getenv("PORT") // 実行時に Heroku が指定するポート番号を取得
 	if len(port) == 0 {
@@ -42,6 +51,7 @@ func main() {
 	}
 	http.HandleFunc("/", handleHome)
 	http.HandleFunc("/secret", handleSecret)
+	http.HandleFunc("/holidays", handleHolidays)
 	log.Printf("ポート %s で待ち受けを開始します...", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Printf("サーバーが異常終了しました: %v", err)
